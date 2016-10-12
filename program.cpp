93e29e207437fa1004eafe58d9feac0817eebc30
rawr <aberry5621@mycom.marin.edu>
2016-10-12 06:38:36 -0700

    Correct range check, remove comments

--- program.cpp
+++ program.cpp
@@ -11,61 +11,59 @@
 using namespace std;
 
 int find_min_number(int input_array[], int array_size, int min_idx, int max_idx) {
-
-	int min_number = input_array[min_idx];
-
-	if ((min_idx < max_idx) && (min_idx > 0) && (max_idx < array_size))
-	{
-		cout << "Searching array from index " << min_idx << " to " << max_idx << endl;
-
-		for (int i = min_idx; i < max_idx; i++) {
-			cout << "Finding min number, examining: " << input_array[i] << endl;
-			if (input_array[i] < min_number) {
-				cout << input_array[i]<< " ..we have sunk to a new low.." << endl;
-				min_number = input_array[i];
-			}
-		}
-	}
-	else 
-	{
-		min_number = -1;
-	}
-
-	return min_number;
-
+    
+    int min_number = input_array[min_idx];
+    
+    if ((min_idx < max_idx) && (min_idx >= 0) && (max_idx <= array_size))
+    {
+        cout << "Searching array from index " << min_idx << " to " << max_idx << endl;
+        
+        for (int i = min_idx; i < max_idx; i++) {
+            
+            if (input_array[i] < min_number) {
+            
+                min_number = input_array[i];
+            }
+        }
+    }
+    else
+    {
+        min_number = -1;
+    }
+    
+    return min_number;
+    
 }
 
 int main() {
-
-	cout << "Print Random Numbers, Find Min Number Between Indexes \n";
-
-	// Initialize data storage
-	const int ARRAY_SIZE = 15;
-	int random_numbers[ARRAY_SIZE] = {0};
-	srand(time(0)); // random seed
-	int arr_size, min_idx = 0, max_idx = 0;
-
-	cout << "Enter indices to start and end search between 0 and 15: ";
-	cin >> min_idx >> max_idx;
-
-	// populate with random numbers between 15 and 55
-	for (int i = 0; i < ARRAY_SIZE; i++) {
-		int rand_number = 15 + (rand() % 40 + 1);
-		random_numbers[i] = rand_number;
-	}
-
-	// then find minimum number in the array
-	int min_number_found_in_range = find_min_number(random_numbers, ARRAY_SIZE, min_idx, max_idx);
-
-	if (min_number_found_in_range != -1)
-	{
-		cout << "Min number is: " << min_number_found_in_range << endl;
-	} else 
-	{
-		cout << "Input error, check input range." << endl;
-	}
-	
-	cout << "~ FIN ~" << endl;
-
-	return 0;
+    
+    cout << "Print Random Numbers, Find Min Number Between Indexes \n";
+    
+    // Initialize data storage
+    const int ARRAY_SIZE = 15;
+    int random_numbers[ARRAY_SIZE] = {0};
+    srand(time(0)); // random seed
+    int arr_size, min_idx = 0, max_idx = 0;
+    
+    cout << "Enter indices to start and end search between 0 and 15: ";
+    cin >> min_idx >> max_idx;
+    
+    // populate with random numbers between 15 and 55
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        int rand_number = 15 + (rand() % 40 + 1);
+        random_numbers[i] = rand_number;
+    }
+    
+    // then find minimum number in the array
+    int min_number_found_in_range = find_min_number(random_numbers, ARRAY_SIZE, min_idx, max_idx);
+    
+    if (min_number_found_in_range != -1)
+    {
+        cout << "Min number is: " << min_number_found_in_range << endl;
+    } else 
+    {
+        cout << "Input error, check input range." << endl;
+    }
+    
+    return 0;
 }
